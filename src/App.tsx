d9d1d139753e576bec9522584c59e12d3b8a92a2
Aykutak04 <aykutak.04.55@gmail.com>
2025-08-02 00:04:16 +0300

    lazy load kaldırıldı

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,41 @@
-import { lazy, Suspense, useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
-
+import Alim from "./components/Alim";
+import Munir from "./components/Munir";
+import Muslum from "./components/Muslum";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-// Sayfaları lazy yükle
-const Alim = lazy(() => import("./components/Alim"));
-// import lazy(Alim) from './components/Alim' meali bu
-
-const Munir = lazy(() => import("./components/Munir"));
-const Muslum = lazy(() => import("./components/Muslum"));
-const MusicAbout = lazy(() => import("./components/MusicAbout"));
-const ShowAllTracks = lazy(() => import("./components/ShowAllTracks"));
-const Aykut = lazy(() => import("./components/Aykut"));
+import MusicAbout from "./components/MusicAbout";
+import ShowAllTracks from "./components/ShowAllTracks";
+import Aykut from "./components/Aykut";
+import { useEffect } from "react";
 
 function App() {
   const navigate = useNavigate();
-
   useEffect(() => {
     console.log("Uygulama açıldığında sadece bir kere çalışır");
+
     navigate("/");
   }, []);
-
   return (
-    <div className="min-h-screen max-container bg-gradient-to-l from-[#2A2A2A] via-[#2a2929] to-[#250b0b2f]">
-      <Navbar />
+    <div className=" min-h-screen max-container bg-gradient-to-l from-[#2A2A2A] via-[#2a2929] to-[#250b0b2f]">
+      <div className="">
+        <Navbar />
+      </div>
+      <div></div>
 
-      <Suspense
-        fallback={
-          <div className="text-white text-center mt-10">Yükleniyor...</div>
-        }
-      >
-        <div className="mt-5">
-          <Routes>
-            <Route path="/" element={<ShowAllTracks />} />
-            <Route path="/adminMusic" element={<Aykut />} />
-            <Route path="/munir" element={<Munir />} />
-            <Route path="/alim" element={<Alim />} />
-            <Route path="/muslum" element={<Muslum />} />
-            <Route path="/musicAbout" element={<MusicAbout />} />
-            <Route path="*" element={<ShowAllTracks />} />
-          </Routes>
-        </div>
-      </Suspense>
+      <div className=" mt-5">
+        <Routes>
+          <Route path="/adminMusic" element={<Aykut />} />
+          <Route path="/" element={<ShowAllTracks />} />
+          <Route path="*" element={<ShowAllTracks />} />
+          <Route path="/munir" element={<Munir />} />
+          <Route path="/alim" element={<Alim />} />
+          <Route path="/muslum" element={<Muslum />} />
+          <Route path="/musicAbout" element={<MusicAbout />} />
+        </Routes>
+      </div>
+
       <div className=" mt-6">
         <Footer />
       </div>
